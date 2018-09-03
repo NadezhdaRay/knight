b1e7c953ceb3930a150617872a3a3b84dca4b3e2
Ivan Grigoriev <if.grigoriev@gmail.com>
2018-09-03 10:14:55 +0300

    Initial Commit

---
+++ Knight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Knight
+//
+//  Created by Ivan Grigoriev on 03/09/2018.
+//  Copyright © 2018 Ivan Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Knight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Knight
+//
+//  Created by Ivan Grigoriev on 03/09/2018.
+//  Copyright © 2018 Ivan Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
